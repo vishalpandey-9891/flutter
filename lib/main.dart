ac03c2801b2ea458e920a815716baa33ca9c2d11
Vishal kumar Pandey <vishalkumar.pandey@dxc.com>
2020-12-20 16:55:22 +0530

    FLUTTER CHAPTER3 COMPLETED

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,7 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
-// this is chapter 2 here we learned about how to most powerful widget - text widgets in
-// flutter application
+// this is chapter 3 here we learned about how to use row & column  widgets - in flutter application
 void main() {
 // runApp() app in flutter to starting point of any application
 // 1)WidgetApp() 2)CupertinoApp  () 3)MaterialApp()
@@ -19,36 +18,37 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("FIRST TEST APP"),
+        title: Text("Awesome App"),
       ),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          width: 100,
-          height: 100,
-          clipBehavior: Clip.antiAlias,
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
+      body: Container(
+        height: 100,
+        color: Colors.teal,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
               color: Colors.red,
-              shape: BoxShape.rectangle,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.pink,
-                  blurRadius: 10,
-                )
-              ],
-              gradient: LinearGradient(colors: [
-                Colors.indigo,
-                Colors.teal,
-              ]),
-              borderRadius: BorderRadius.circular(8)),
-          child: Text("I am a box",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              )),
+              alignment: Alignment.center,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.yellow,
+              alignment: Alignment.center,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.green,
+              alignment: Alignment.center,
+            ),
+          ],
         ),
       ),
     );
