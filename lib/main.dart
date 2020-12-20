bddf349d37af66ddb8bd9ba17d67e32e9cf12fe5
Vishal kumar Pandey <vishalkumar.pandey@dxc.com>
2020-12-20 16:36:36 +0530

    FLUTTER CHAPTER 2 COMPLETED

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
-// this is chapter 1 here we learned about how to use text,container and scanffold widgets in
+// this is chapter 2 here we learned about how to most powerful widget - text widgets in
 // flutter application
 void main() {
 // runApp() app in flutter to starting point of any application
@@ -13,17 +13,6 @@ void main() {
   ));
 }
 
-// SHOW TEXT USING CLASS HOMEPAGE IN APP
-// class HomePage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       color: Colors.deepOrange,
-//       child: Text("Hi flutter APP"),
-//     );
-//   }
-// }
-
 // stateless widget means nothing changes here
 class HomePage extends StatelessWidget {
   @override
@@ -32,8 +21,35 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("FIRST TEST APP"),
       ),
-      body: Container(
-        child: Text("hi flutter"),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          width: 100,
+          height: 100,
+          clipBehavior: Clip.antiAlias,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: Colors.red,
+              shape: BoxShape.rectangle,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.pink,
+                  blurRadius: 10,
+                )
+              ],
+              gradient: LinearGradient(colors: [
+                Colors.indigo,
+                Colors.teal,
+              ]),
+              borderRadius: BorderRadius.circular(8)),
+          child: Text("I am a box",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              )),
+        ),
       ),
     );
   }
