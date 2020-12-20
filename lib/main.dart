57ceefcc4d96b376a19e685796f7711ce4b241ab
Vishal kumar Pandey <vishalkumar.pandey@dxc.com>
2020-12-20 17:32:15 +0530

    chapter 4 drawer & floating action button complete

--- lib/main.dart
+++ lib/main.dart
@@ -31,29 +31,33 @@ class HomePage extends StatelessWidget {
         child: ListView(
           padding: EdgeInsets.all(0),
           children: <Widget>[
-            DrawerHeader(
-              child: Text("MAIN MENU"),
-              decoration: BoxDecoration(
-                color: Colors.purple,
-              ),
-            ),
+            UserAccountsDrawerHeader(
+                accountName: Text("vishal  pandey"),
+                accountEmail: Text("vi@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://images.unsplash.com/photo-1497551060073-4c5ab6435f12?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"),
+                )),
             ListTile(
               leading: Icon(Icons.person),
               title: Text(" vishal pandey"),
               subtitle: Text("developer"),
               trailing: Icon(Icons.edit),
+              onTap: () {},
             ),
             ListTile(
               leading: Icon(Icons.email),
               title: Text("Mail id "),
               subtitle: Text("vishalpandey@gmail.com"),
               trailing: Icon(Icons.edit),
+              onTap: () {},
             ),
             ListTile(
               leading: Icon(Icons.location_city),
               title: Text(" ADDRESS"),
               subtitle: Text("my address z320b sector 12"),
               trailing: Icon(Icons.edit),
+              onTap: () {},
             )
           ],
         ),
