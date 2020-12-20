262913837daa4d8e48892557a8cafb697fd34fe6
Vishal kumar Pandey <vishalkumar.pandey@dxc.com>
2020-12-20 17:22:09 +0530

    chapter 4 started

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
-// this is chapter 3 here we learned about how to use row & column  widgets - in flutter application
+// this is chapter 4 here we learned about how to use Material Drawer | ListView | Circle Avatar | NetworkImage | Floating Action Button ⛳️  widgets - in flutter application
 void main() {
 // runApp() app in flutter to starting point of any application
 // 1)WidgetApp() 2)CupertinoApp  () 3)MaterialApp()
@@ -20,36 +20,47 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Container(
-        height: 100,
-        color: Colors.teal,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: Center(
+        child: Container(
+          height: 100,
+          width: 100,
+          color: Colors.teal,
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.all(0),
           children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(8),
-              width: 100,
-              height: 100,
-              color: Colors.red,
-              alignment: Alignment.center,
+            DrawerHeader(
+              child: Text("MAIN MENU"),
+              decoration: BoxDecoration(
+                color: Colors.purple,
+              ),
             ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              width: 100,
-              height: 100,
-              color: Colors.yellow,
-              alignment: Alignment.center,
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text(" vishal pandey"),
+              subtitle: Text("developer"),
+              trailing: Icon(Icons.edit),
             ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              width: 100,
-              height: 100,
-              color: Colors.green,
-              alignment: Alignment.center,
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("Mail id "),
+              subtitle: Text("vishalpandey@gmail.com"),
+              trailing: Icon(Icons.edit),
             ),
+            ListTile(
+              leading: Icon(Icons.location_city),
+              title: Text(" ADDRESS"),
+              subtitle: Text("my address z320b sector 12"),
+              trailing: Icon(Icons.edit),
+            )
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.edit),
       ),
     );
   }
